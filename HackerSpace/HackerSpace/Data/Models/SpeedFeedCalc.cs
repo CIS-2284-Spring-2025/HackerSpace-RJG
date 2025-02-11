b79b6f21097aa07ecedb47f3ec0f125585c210a0
Robert Garner <rgarner011235@gmail.com>
2025-02-11 11:16:43 -0700

    Started work on using Blazor components

---
+++ HackerSpace/HackerSpace/Data/Models/SpeedFeedCalc.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HackerSpace.Data.Models
+{
+    public class SpeedFeedCalc
+    {
+        private double rpm;
+
+        [Required]
+        [Range(0, 100000)]
+        public double RPM 
+        {
+            get { return rpm; }
+            set { rpm = value; CalculateSFM(); }
+        }
+
+        private double diam;
+        [Required]
+        [Range(0,double.MaxValue)]
+        public double Diam
+        {
+            get { return diam; }
+            set { diam = value; CalculateSFM(); }
+        }
+
+        public double SFM { get; private set; }
+
+        private void CalculateSFM()
+        {
+            SFM = rpm * diam / 3.82;
+        }
+    }
+}
