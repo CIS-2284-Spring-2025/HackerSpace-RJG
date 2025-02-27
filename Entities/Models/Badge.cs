a32e74f1a4498b33cfb520ea6a8c8a4f4f457166
Robert Garner <rgarner011235@gmail.com>
2025-02-27 11:11:49 -0700

    Completed detail page and list view and edit view

--- Entities/Models/Badge.cs
+++ Entities/Models/Badge.cs
@@ -1,4 +1,6 @@
-﻿namespace Entities.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Entities.Models
 {
     /// <summary>
     /// Badge describes badges students can earn
@@ -10,19 +12,26 @@
         /// </summary>
         public Guid Id { get; set; }
         /// <summary>
-        /// Title for the badge
+        /// Title for the badge.
+        /// Required and must have 5 characters.
         /// </summary>
+        [Required]
+        [MinLength(5)]
         public string? Title { get; set; }
         /// <summary>
-        /// Description to the student on what the badge is for.
+        /// Description to the student on what the badge is for. Required and must have 20 characters.
         /// </summary>
-        public string? Description { get; set; }        
+        [Required]
+        [MinLength(20)]
+        public string? Description { get; set; }
         /// <summary>
-        /// Instructions to the student on how to get credit for their work.
+        /// Instructions to the student on how to get credit for their work. Required and must have 20 characters.
         /// </summary>
+        [Required]
+        [MinLength(20)]
         public string? TurnInInstructions { get; set; }
         /// <summary>
-        /// Badge is not displayed to students until this is true.
+        /// Badge is not displayed to students until this is true. Required and must have 20 characters.
         /// </summary>
         public bool? IsVisible { get; set; }
     }
