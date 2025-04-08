fd724fbc99d26881ac36989d3fc49861fc2c7322
Robert Garner <rgarner011235@gmail.com>
2025-04-08 10:16:44 -0600

    started adding rocky lohtkas solution

--- HackerSpace/HackerSpace.Client/Program.cs
+++ HackerSpace/HackerSpace.Client/Program.cs
@@ -12,16 +12,21 @@ namespace HackerSpace.Client
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
-            builder.Services.AddAuthorizationCore();
-            builder.Services.AddCascadingAuthenticationState();
-            builder.Services.AddSingleton<AuthenticationStateProvider, PersistentAuthenticationStateProvider>();
-
             //For Api calls
             builder.Services.AddTransient(sp =>
             new HttpClient
             {
                 BaseAddress = new Uri(builder.Configuration["FrontendUrl"] ?? "https://localhost:7170")
             });
+            // TODO: Try Rocky's sollution bellow from https://blog.lhotka.net/2024/10/13/Accessing-User-Identity-on-a-Blazor-Wasm-Client
+            //builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+            builder.Services.AddAuthorizationCore();
+
+
+            builder.Services.AddCascadingAuthenticationState();
+            //builder.Services.AddSingleton<AuthenticationStateProvider, PersistentAuthenticationStateProvider>();  
+            builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthenticationStateProvider>(); //To get userid         
 
             //Add data services
             builder.Services.AddTransient<IBadgesPageDataService, BadgesPageDataService>();
